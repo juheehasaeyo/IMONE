cc31388d3f5d1f99ff0afcfec8189d05472a04fa
jungwoo3490 <jungwoo3490@naver.com>
2023-05-12 18:03:31 +0900

    출력부 수정 완료

--- MPU6050/bluetoothParser.c
+++ MPU6050/bluetoothParser.c
@@ -1,40 +1,50 @@
 #include <SoftwareSerial.h>
-#include<Wire.h>
+#include <Wire.h>
 
-const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 
 SoftwareSerial bluetooth(4, 3);
- 
-void setup(){
+
+void setup()
+{
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0);    // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   Serial.begin(9600);
   bluetooth.begin(9600);
 }
 
-void loop(){
+void loop()
+{
   Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  bluetooth.print("AcX = "); bluetooth.print(AcX);
-  bluetooth.print(" | AcY = "); bluetooth.print(AcY);
-  bluetooth.print(" | AcZ = "); bluetooth.print(AcZ);
-  bluetooth.print(" | Tmp = "); bluetooth.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-  bluetooth.print(" | GyX = "); bluetooth.print(GyX);
-  bluetooth.print(" | GyY = "); bluetooth.print(GyY);
-  bluetooth.print(" | GyZ = "); bluetooth.print(GyZ);
+  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  bluetooth.print("가속도 (");
+  bluetooth.print(AcX);
+  bluetooth.print(",");
+  bluetooth.print(AcY);
+  bluetooth.print(",");
+  bluetooth.print(AcZ);
+  bluetooth.print(")    /   ");
+  bluetooth.print("각속도 (");
+  bluetooth.print(GyX);
+  bluetooth.print(",");
+  bluetooth.print(GyY);
+  bluetooth.print(",");
+  bluetooth.print(GyZ);
+  bluetooth.print(")");
+  Serial.println(" ");
   Serial.println(" ");
   delay(500);
 }
